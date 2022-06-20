5ce7ba0b309f2e613b615062dceba34fe379f839
김응호 <keh_as@naver.com>
2022-06-20 21:13:27 +0900

    대규모 리뉴얼 / 각 레이아웃 위치 변경 / 오른쪽 메모 레이아웃 추가 / 상단 배너, 하단 배너 레이아웃 추가

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,41 +2,59 @@
 
 let cal = {
     $el: {
-        // button
+        // calendar
         calendar_prev: $('button[data-action=move-prev]'),
         calendar_next: $('button[data-action=move-next]'),
         calendar_today: $('button[data-action=move-today]'),
 
+        // 통계
+        statistics: $('button[id=statistics]'),
+
+        // 메모
+        memo_save: $('button[id=memo_save]'),
+        memo: $('textarea'),
+
         // text
         calendar_date: $('span[id=renderRange]')
+
     },
     init: function() {
         this.event();
 
-        let today = new Date(calendar._renderDate._date);
+        let today = new Date(calendar._renderDate._date)
         this.$el.calendar_date.text(today.getFullYear() + "년 " + (today.getMonth() + 1) + "월")
+
+        this.$el.memo.text("")
     },
     event: function() {
-        that = this;
-        this.$el.calendar_next.click(function(){
+        let that = this;
+        that.$el.calendar_next.click(function(){
             calendar.next();
 
-            let next = new Date(calendar._renderDate._date);
+            let next = new Date(calendar._renderDate._date)
             that.$el.calendar_date.text(next.getFullYear() + "년 " + (next.getMonth() + 1) + "월")
         })
 
-        this.$el.calendar_prev.click(function(){
+        that.$el.calendar_prev.click(function(){
             calendar.prev();
 
-            let prev = new Date(calendar._renderDate._date);
+            let prev = new Date(calendar._renderDate._date)
             that.$el.calendar_date.text(prev.getFullYear() + "년 " + (prev.getMonth() + 1) + "월")
         })
 
-        this.$el.calendar_today.click(function(){
+        that.$el.calendar_today.click(function(){
             calendar.today();
 
-            let today = new Date(calendar._renderDate._date);
+            let today = new Date(calendar._renderDate._date)
             that.$el.calendar_date.text(today.getFullYear() + "년 " + (today.getMonth() + 1) + "월")
+        })
+
+        that.$el.statistics.click(function() {
+            alert('통계 준비 중입니다.')
+        })
+
+        that.$el.memo_save.click(function () {
+            alert('메모 저장 준비 중입니다.')
         })
     }
 }
