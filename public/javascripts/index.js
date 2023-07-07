1b60c0f1f9c22e7c226d9042ffb999b190a84fdf
eungho <keh_as@naver.com>
2023-07-07 14:34:37 +0900

    D-Day 추가

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -7,6 +7,9 @@ let cal = {
         calendar_next: $('button[data-action=move-next]'),
         calendar_today: $('button[data-action=move-today]'),
 
+        //d-day
+        calendar_day: $('label[id=d_day]'),
+
         // 통계
         statistics: $('button[id=statistics]'),
 
@@ -27,14 +30,15 @@ let cal = {
 
         this.$el.memo.text("")
 
-        this.event();
-        this.table();
+        this.event()
+        this.table()
+        this.day()
     },
     table: function() {
-        that = this;
+        that = this
 
-        let tbody = "";
-        let count = 1;
+        let tbody = ""
+        let count = 1
         $.post('/grap/api/select/region', function(data){
             if(data.mode) {
                 for(let a of data.result) {
@@ -51,23 +55,23 @@ let cal = {
         })
     },
     event: function() {
-        let that = this;
+        let that = this
         that.$el.calendar_next.click(function(){
-            calendar.next();
+            calendar.next()
 
             let next = new Date(calendar._renderDate._date)
             that.$el.calendar_date.text(next.getFullYear() + "년 " + (next.getMonth() + 1) + "월")
         })
 
         that.$el.calendar_prev.click(function(){
-            calendar.prev();
+            calendar.prev()
 
             let prev = new Date(calendar._renderDate._date)
             that.$el.calendar_date.text(prev.getFullYear() + "년 " + (prev.getMonth() + 1) + "월")
         })
 
         that.$el.calendar_today.click(function(){
-            calendar.today();
+            calendar.today()
 
             let today = new Date(calendar._renderDate._date)
             that.$el.calendar_date.text(today.getFullYear() + "년 " + (today.getMonth() + 1) + "월")
@@ -80,12 +84,24 @@ let cal = {
             }
             $.post('/dashboard/api/add', param, function (data) {
                 if (data.mode) {
-                    toastr.success(data.result);
+                    toastr.success(data.result)
                 } else {
-                    toastr.success(data.result);
+                    toastr.success(data.result)
                 }
             })
         })
+    },
+    day: function() {
+        const setDate = new Date('2021-10-19')
+        const now = new Date();
+
+        const Time = setDate.getTime() - now.getTime()
+        const dday = Math.abs(Time / (1000 * 60 * 60 * 24)) // 밀리터리 * 초 * 분 * 시 = 일
+
+        // 밀리터리는 1초당 1000 단위로 나타냄
+        // 시계는 1초마다 움직이므로 1000 으로 줘야함
+
+        this.$el.calendar_day.text("♥" + parseInt(dday + 1) + "♥" )
     }
 }
 
